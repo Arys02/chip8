162049508a0305a0f54555229d02997916fb83cb
Charles Yaiche <yaiche_c@epita.fr>
2017-11-26 23:34:24 +0000

    [UPDATE] remove all opcode operation to another file

--- src/cpu.h
+++ src/cpu.h
@@ -1,8 +1,8 @@
 #ifndef CPU_H
 # define CPU_H
+
 #define MEMSIZE 4092
 #define ADDRSTART 512
-#define NBOPCODE 35
 
 typedef struct
 {
@@ -22,21 +22,9 @@ typedef struct
 
 } CPU;
 
-typedef struct
-{
-  Uint16 mask[NBOPCODE];
-  Uint16 id[NBOPCODE];
-
-} JUMP;
-
 void cpu_init(CPU *cpu);
 void cpu_decompter(CPU *cpu);
 void cpu_print(CPU *cpu);
-
-
-void init_jump(JUMP *jp);
-Uint16 get_opcode(CPU *cpu);
-void decode_opcode(CPU *cpu);
 
 
 #endif /* !CPU_H */
