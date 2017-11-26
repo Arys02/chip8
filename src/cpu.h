bc1e41b454f218788983b206cacb70d5acf65f49
Charles Yaiche <yaiche_c@epita.fr>
2017-11-26 18:17:24 +0000

    [ADD] cpu.{h|c} : jump struct, init_jump, get_opcode, decode_opcode

--- src/cpu.h
+++ src/cpu.h
@@ -2,6 +2,7 @@
 # define CPU_H
 #define MEMSIZE 4092
 #define ADDRSTART 512
+#define NBOPCODE 35
 
 typedef struct
 {
@@ -21,9 +22,21 @@ typedef struct
 
 } CPU;
 
+typedef struct
+{
+  Uint16 mask[NBOPCODE];
+  Uint16 id[NBOPCODE];
+
+} JUMP;
+
 void cpu_init(CPU *cpu);
 void cpu_decompter(CPU *cpu);
 void cpu_print(CPU *cpu);
+
+
+void init_jump(JUMP *jp);
+Uint16 get_opcode(CPU *cpu);
+void decode_opcode(CPU *cpu);
 
 
 #endif /* !CPU_H */
--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include "screen.h"
 #include "sdl_tools.h"
 
+#define CPUSPEED 4
+#define FRAMERATE 16
+
 int main()
 {
   //setup CPU
@@ -18,7 +21,18 @@ int main()
   SCREEN *screen = malloc(sizeof(SCREEN));
   screen_init(screen);
 
-  screen_update(screen);
+
+  do
+  {
+
+    //op 1
+    //op 2
+    //op 3
+    //op 4
+    screen_update(screen);
+    SDL_Delay(FRAMERATE);
+  } while(1);
+
 
   sdl_break();
   sdl_quit(screen);
